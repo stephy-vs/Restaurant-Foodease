0e6a8673ddd2398e540ebcaf14cc45fe2f8a2c10
stephy-VS <vsstephy.ptf@gmail.com>
2023-11-15 10:04:22 -0500

    update user status

--- src/main/java/com/Foodease/FoodeaseApp/rest/UserRest.java
+++ src/main/java/com/Foodease/FoodeaseApp/rest/UserRest.java
@@ -2,10 +2,7 @@ package com.Foodease.FoodeaseApp.rest;
 
 import com.Foodease.FoodeaseApp.wrapper.UserWrapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -20,7 +17,7 @@ public interface UserRest {
 
     @GetMapping(path = "/get")
     public ResponseEntity<List<UserWrapper>> getAllUser();
-    @PostMapping(path = "/update")
+    @PutMapping(path = "/update")
     public ResponseEntity<String> update(@RequestBody(required = true) Map<String,String> requestMap);
 
 }
